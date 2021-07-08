91b94c72c704f1142d30490806af9b530314f23c
maxinth <tunde.j.adeleye@gmail.com>
2021-07-08 16:46:47 +0100

    added innerContainer

--- src/components/HomePageComps/NavBar/MainNav/index.tsx
+++ src/components/HomePageComps/NavBar/MainNav/index.tsx
@@ -1,12 +1,19 @@
-import { Container } from "./styled";
+import { Container,InnerContainer } from "./styled";
 import Logo from "./Logo";
 
 const MainNav = () => {
   return (
     <Container>
+     <InnerContainer>
       <Logo />
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
+      perspiciatis laboriosam esse eveniet perferendis, optio odio rerum labore
+      commodi praesentium, ad minus deleniti exercitationem dicta ratione sit
+      error modi similique alias earum quaerat officia quae. A blanditiis
+      voluptate nam? Autem.
+     </InnerContainer>
     </Container>
-  );
+  ); 
 };
 
 export default MainNav;
--- src/components/HomePageComps/NavBar/MainNav/styled.ts
+++ src/components/HomePageComps/NavBar/MainNav/styled.ts
@@ -1,14 +1,31 @@
 import styled from "styled-components";
 
-const Container = styled.section``;
+const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const Image = styled.img`
   object-fit: contain;
   width: 50px;
   height: 50px;
+  :first-child {
+    width: 150px;
+    height: 150px;
+  }
 `;
-const LogoContainer = styled.div`
+const LogoContainer = styled.section`
   display: flex;
   /* justify-content: center; */
   align-items: center;
+  position: relative;
+  top: -43px;
+  
 `;
-export { Container, Image, LogoContainer };
+
+const InnerContainer = styled.div`
+margin: 0 auto;
+  max-width: 900px;
+  width: 100%;
+`;
+export { Container, Image, LogoContainer, InnerContainer };
