96ab462943a884f30f2d4c072c02d4315de2f214
maxinth <tunde.j.adeleye@gmail.com>
2021-07-09 16:33:29 +0100

    rolled back to an earlier version of the RightOptions component

--- src/components/HomePageComps/NavBar/MainNav/Logo.tsx
+++ src/components/HomePageComps/NavBar/MainNav/Logo.tsx
@@ -1,11 +1,11 @@
 import FlagImage from "../../../../assets/flag.jpg";
-import LogoImage from "../../../../assets/toyota.png";
+// import LogoImage from "../../../../assets/download.jfif";
 import { Image, LogoContainer } from "./styled";
 
 const Logo = () => {
   return (
     <LogoContainer>
-      <Image src={LogoImage} alt="logoImage" />
+      {/* <Image src={LogoImage} alt="logoImage" /> */}
       <Image src={FlagImage} alt="FlagImage" />
     </LogoContainer>
   );
