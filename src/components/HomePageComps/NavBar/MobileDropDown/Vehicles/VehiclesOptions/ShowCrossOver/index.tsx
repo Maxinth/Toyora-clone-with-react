c2651899af96f9ecba5a91112f3164d7a30ef303
maxinth <tunde.j.adeleye@gmail.com>
2021-07-25 18:29:50 +0100

    added initial data for CrossOver cars

--- src/components/HomePageComps/NavBar/MobileDropDown/Vehicles/VehiclesOptions/ShowCrossOver/index.tsx
+++ src/components/HomePageComps/NavBar/MobileDropDown/Vehicles/VehiclesOptions/ShowCrossOver/index.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import VehicleOptionsLayout from "../VehicleOptionsLayout";
+import useVehicleState from "../useVehicleState";
+import { data } from "./data";
 
 const ShowCrossOver = () => {
-  return <div></div>;
+  const { isInView } = useVehicleState("crossOver");
+  return (
+    <VehicleOptionsLayout
+      isInView={isInView}
+      data={data}
+      topTitle="crossovers & suvs"
+    />
+  );
 };
 
 export default ShowCrossOver;
--- src/components/HomePageComps/NavBar/MobileDropDown/Vehicles/VehiclesOptions/index.tsx
+++ src/components/HomePageComps/NavBar/MobileDropDown/Vehicles/VehiclesOptions/index.tsx
@@ -1,11 +1,13 @@
 import ShowCars from "./ShowCars";
 import ShowTrucks from "./ShowTrucks";
+import ShowCrossOver from "./ShowCrossOver";
 
 const VehicleOptions = () => {
   return (
     <>
       <ShowCars />
       <ShowTrucks />
+      <ShowCrossOver />
     </>
   );
 };
