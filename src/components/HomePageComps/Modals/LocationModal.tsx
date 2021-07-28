7c5ff65a10f32058d4f457958c582adb6f3f342b
maxinth <tunde.j.adeleye@gmail.com>
2021-07-28 15:18:06 +0100

    Location modal - first go

---
+++ src/components/HomePageComps/Modals/LocationModal.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LocationModal = () => {
+  return <div>Location modal !</div>;
+};
+
+export default LocationModal;
--- src/components/HomePageComps/index.tsx
+++ src/components/HomePageComps/index.tsx
@@ -1,11 +1,8 @@
 import { HomeContainer } from "./styled";
 import Navbar from "./NavBar";
-// import MockComponent from "./MockComponents";
-// import {useSelector} from 'react-redux'
 import MobileDropDown from "./NavBar/MobileDropDown";
-// import {RootState} from "../../appStore/store"
+
 const HomePage = () => {
-  // const fullView = useSelector((state:RootState) => state.vehicles.vehiclesInView)
   return (
     <HomeContainer>
       <Navbar />
