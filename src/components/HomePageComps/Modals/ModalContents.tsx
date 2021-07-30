f8df0efd54684e190244ef8226b46bf1e00b3598
maxinth <tunde.j.adeleye@gmail.com>
2021-07-30 10:21:33 +0100

    added transitions to closeIcon

--- src/components/HomePageComps/Modals/ModalContents.tsx
+++ src/components/HomePageComps/Modals/ModalContents.tsx
@@ -1,10 +1,17 @@
-import { Container, Text } from "./styled";
+import { Container, Text, TextBox, Input, Submit } from "./styled";
 import ModalTopControl from "./ModalTopControl";
 const ModalContents = () => {
   return (
     <Container>
       <ModalTopControl />
-      <Text>Enter your Zip code to find dealers, inventory and special offers near you</Text>
+      <Text normal>
+        Enter your Zip code to find dealers, inventory and special offers near
+        you
+      </Text>
+      <TextBox>
+        <Input type="text" />
+        <Submit>submit</Submit>
+      </TextBox>
     </Container>
   );
 };
--- src/components/HomePageComps/Modals/styled.ts
+++ src/components/HomePageComps/Modals/styled.ts
@@ -1,9 +1,8 @@
-import styled,{css} from "styled-components";
+import styled, { css } from "styled-components";
 import { motion } from "framer-motion";
 
-
 interface TextProps {
-  normal?:boolean;
+  normal?: boolean;
 }
 const BackDrop = styled(motion.div)`
   position: absolute;
@@ -12,6 +11,7 @@ const BackDrop = styled(motion.div)`
   top: 0;
   bottom: 0;
   /* height: 100vh; */
+  //set background in framer variant
   /* background: rgba(0, 0, 0, 0.7); */
   z-index: 900;
   cursor: pointer;
@@ -20,6 +20,7 @@ const BackDrop = styled(motion.div)`
   justify-content: center;
   align-items: center;
   transition: padding 0.2s;
+  width: 100%;
   @media (min-width: 768px) {
     padding: unset;
   }
@@ -29,9 +30,15 @@ const Container = styled.div`
   background-color: #fff;
   height: 100%;
   max-width: 900px;
+  width: 100%;
   margin: 0 auto;
   padding: 0.5rem;
   transition: height 0.2s;
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: center;
+  font-family: "Raleway", sans-serif;
   @media (min-width: 768px) {
     height: 70%;
   }
@@ -42,20 +49,44 @@ const ControlBox = styled.div`
   justify-content: space-between;
   align-items: center;
   padding: 1rem;
+
   font-family: "Raleway", sans-serif;
+  margin-bottom: 3rem;
   & svg {
     font-size: 2.4rem;
+    transition: transform 0.2s;
+  }
+
+  & svg:hover {
+    transform: scale(1.5);
   }
 `;
 
 const Text = styled.p<TextProps>`
   font-weight: bold;
   font-size: 1.3rem;
-  ${({normal}) => normal && css`
-  font-weight:normal
-  font-size: 1rem;
-  `}
+  ${({ normal }) =>
+    normal &&
+    css`
+      font-weight: 600;
+      font-size: 1rem;
+      text-align: center;
+      max-width: 300px;
+      margin: 0 auto;
+      color: grey;
+      line-height: 1.9;
+    `}
 `;
 
+const TextBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > * {
+    flex: 1;
+  }
+`;
+const Input = styled.input``;
+const Submit = styled.div``;
 
-export { BackDrop, Container, ControlBox, Text };
+export { TextBox, Input, Submit, BackDrop, Container, ControlBox, Text };
