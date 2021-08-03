891e448177cdbe6c3c123b0c9cbbab8ae837cef4
maxinth <tunde.j.adeleye@gmail.com>
2021-08-03 22:00:14 +0100

    reverted to initial view

--- src/components/HomePageComps/BannerSlider/index.tsx
+++ src/components/HomePageComps/BannerSlider/index.tsx
@@ -5,7 +5,51 @@ const BannerSlider = () => {
   return (
     <Container>
       <Image src={ImgOne} alt="venza" />
-      <div>slider goes here</div>
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio minus,
+        veniam nesciunt aut saepe, eum culpa explicabo, corporis quo laudantium
+        recusandae qui reprehenderit assumenda corrupti inventore voluptate
+        provident praesentium vero. Quod facilis, totam eos ea perferendis
+        minima, ipsa, obcaecati tempore quam voluptas hic eaque maiores! Iste
+        excepturi, earum facilis blanditiis consectetur velit eum id dolorem non
+        cum ipsum dolorum explicabo numquam reprehenderit odit corrupti incidunt
+        nihil minima laudantium illum accusamus recusandae perspiciatis? Nobis
+        iure quis maiores provident accusantium neque dolore quidem expedita
+        obcaecati rem at, quae harum ipsum iusto, ducimus nam maxime nihil
+        nesciunt ex suscipit dolores deleniti, aperiam natus! Maiores cumque
+        necessitatibus autem ducimus minima a ullam nostrum quod temporibus
+        ipsam modi, eos ratione commodi ipsum dolores facere asperiores
+        incidunt! Fugit nostrum dolorum possimus inventore. Aliquid atque iure
+        assumenda magnam quos maxime inventore ullam voluptatem optio autem
+        nihil incidunt explicabo sed, commodi possimus accusantium impedit
+        architecto delectus fugiat quod facilis aspernatur iste! Repudiandae
+        rerum temporibus quis nam? Tenetur rerum eos atque dicta iure officia,
+        amet voluptates, saepe itaque a facere! Nisi hic dolor vel fugiat id
+        incidunt ad officia nulla esse quibusdam, accusamus commodi ipsa qui
+        nobis placeat fuga ducimus tempore sint dolorum. Tenetur praesentium non
+        atque sed quia illo, recusandae, veniam pariatur numquam ipsum
+        doloremque! Molestiae aperiam deleniti cupiditate beatae autem sed id,
+        nemo, ratione in commodi architecto non? Molestiae esse provident quia
+        quibusdam eligendi ex. Voluptatem id quam nostrum laboriosam quae, illum
+        eos animi at blanditiis ut sapiente excepturi! Ad cumque, ullam animi
+        nesciunt maxime alias? Delectus minima est cupiditate aliquam illum quia
+        illo sunt atque molestiae quod in ab earum iure assumenda impedit libero
+        voluptatum a praesentium officia, et eum laudantium totam voluptatem?
+        Ipsa at est magnam sed quidem quaerat voluptate officia hic error
+        accusantium! Ab animi quisquam unde omnis. Provident, cupiditate soluta
+        incidunt, debitis iste reprehenderit placeat nulla, modi esse
+        necessitatibus sed labore consequatur dolores voluptatibus est ipsam
+        dolorem neque blanditiis minus quia ea. Blanditiis fugit saepe,
+        veritatis tempore nisi accusamus dolor beatae modi, quia, unde est! Vero
+        qui magnam numquam nesciunt odit et, maxime voluptatem ducimus mollitia
+        perferendis dolores ab libero atque, aspernatur beatae nihil nulla.
+        Adipisci reprehenderit nostrum harum consectetur soluta expedita, nihil,
+        sequi ab natus, vero doloribus ratione omnis aliquid? Nobis aspernatur
+        laboriosam saepe nostrum aperiam rem, ex quidem sunt, eum, ipsam omnis
+        optio voluptates delectus inventore natus explicabo? Inventore quaerat
+        ipsam odio molestiae? Repellendus a accusamus, architecto sed
+        voluptatibus consectetur!
+      </div>
     </Container>
   );
 };
