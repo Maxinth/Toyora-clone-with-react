7775edd2bee9ad6779292f4bceea3df242c8c728
maxinth <tunde.j.adeleye@gmail.com>
2021-08-03 21:58:57 +0100

    reverted to innitial vew

--- src/components/HomePageComps/BannerSlider/index.tsx
+++ src/components/HomePageComps/BannerSlider/index.tsx
@@ -5,51 +5,7 @@ const BannerSlider = () => {
   return (
     <Container>
       <Image src={ImgOne} alt="venza" />
-      <div>
-        unde nisi, cupiditate sint! Laudantium hic et doloremque dolores at
-        alias, delectus laboriosam error dolor nesciunt consequatur asperiores
-        atque nisi voluptatem natus nulla cupiditate tempore accusantium
-        pariatur fugit reiciendis id expedita exercitationem perspiciatis?
-        Officia mollitia dolores ut nemo voluptatem quasi iusto necessitatibus
-        nisi ex facere molestiae fuga soluta eos voluptatibus excepturi neque,
-        eaque sapiente sit pariatur, laudantium ducimus ad blanditiis.
-        Consequatur pariatur aliquam, eaque est hic voluptates voluptatem libero
-        expedita vitae repellendus non, ex quidem modi alias eius voluptatibus
-        debitis! Reprehenderit nemo sequi veritatis rem quibusdam repellat,
-        earum ipsam dolore eum, totam ex quasi perspiciatis dolorem doloremque
-        est, possimus quia! Soluta, doloremque voluptate doloribus modi maxime
-        omnis a nisi numquam assumenda laborum! Ad fuga eligendi temporibus, rem
-        eos et dolore ipsum laboriosam fugit quaerat blanditiis earum recusandae
-        repellendus. Dignissimos quaerat soluta est odit molestiae molestias
-        dolorum! Sit fugit fuga pariatur in, assumenda facilis. Ea ab ratione
-        dolorum. Iure nam rerum beatae recusandae maiores, hic cupiditate
-        explicabo ullam eius quia provident veritatis consectetur neque vitae
-        ipsa corporis obcaecati dolore illum tempore. Corrupti expedita quod,
-        ab, laudantium quam rem libero ipsum accusamus iste tenetur, aut saepe
-        ex corporis quia optio. At recusandae a, tempore reprehenderit
-        exercitationem porro aliquam laborum obcaecati, iusto sapiente sequi ad!
-        Delectus totam optio sit facere quaerat quibusdam expedita sequi,
-        dignissimos beatae omnis. Distinctio veritatis ut ullam doloribus hic
-        harum inventore quod perferendis earum. Expedita et distinctio,
-        molestiae nemo ullam consequuntur vero omnis at fugiat molestias!
-        Aperiam magnam ducimus tenetur deleniti praesentium quod cum aliquid
-        fuga impedit eos, iusto blanditiis, soluta quia illo nesciunt id velit
-        non earum saepe corporis. Sunt laboriosam quo similique, laborum
-        deleniti ipsam, mollitia iure officiis harum, saepe et deserunt vel non.
-        Debitis et illo quos velit tempora. Consectetur, qui ratione, corrupti
-        officia iusto vitae quasi quisquam dolorem similique, iure ut.
-        Voluptate, reiciendis ullam, ipsum nostrum est similique ad adipisci
-        laboriosam soluta quam iste placeat autem deleniti asperiores, rem
-        possimus maxime alias ipsa dolorem labore animi saepe molestias in?
-        Tempora facere et nostrum quidem fugiat. Facere repellat tempora
-        reiciendis cum sunt omnis odio. Unde sed incidunt voluptates numquam
-        suscipit! Ipsa eveniet aspernatur quos, non ad nostrum neque tenetur
-        dignissimos sunt aut. Sed ex explicabo placeat omnis eius voluptas quae,
-        est nisi dicta soluta dolores, aliquam nihil quasi a quisquam ullam sint
-        quod earum cupiditate et, unde adipisci iste excepturi! Quae ipsum
-        beatae dolorum sunt atque. Rem eius iste assumenda sit animi,
-        necessitatibus quibusdam beatae deleniti quod.
-      </div>
+      <div>slider goes here</div>
     </Container>
   );
 };
